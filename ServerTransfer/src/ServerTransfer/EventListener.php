c7c7c355f6f53294bd671a59c741d0f95c0f24c0
GamakCZ <gamak.mcpe@gmail.com>
2017-05-07 18:59:25 +0200

    Update EventListener.php

--- ServerTransfer/src/ServerTransfer/EventListener.php
+++ ServerTransfer/src/ServerTransfer/EventListener.php
@@ -18,15 +18,20 @@ class EventListener implements Listener {
         $player = $event->getPlayer();
         $cmd = explode(" ", strtolower($event->getMessage()));
         if($cmd[0] == "/transfer" || $cmd[0] == "/transferserver") {
+
             if(isset($cmd[1]) && isset($cmd[2])) {
-                $this->plugin->transfer($player,$cmd[1], $cmd[2]);
-                $this->plugin->getServer()->broadcastMessage(str_replace(":19132","","[Transfer] Player {$player->getName()} is transfered to {$cmd[1]}:{$cmd[2]}"));
+                if($player->hasPermission("st.transfer")) {
+                    $this->plugin->transfer($player,$cmd[1], $cmd[2]);
+                    $this->plugin->getServer()->broadcastMessage(str_replace(":19132","","[Transfer] Player {$player->getName()} is transfered to {$cmd[1]}:{$cmd[2]}"));
+                }
             }
             elseif (isset($cmd[1]) && isset($cmd[2]) && isset($cmd[3])) {
-                $pl = $this->plugin->getServer()->getPlayer($cmd[3]);
-                if($pl->isOnline()) {
-                    $this->plugin->transfer($pl, $cmd[1], $cmd[2]);
-                    $this->plugin->getServer()->broadcastMessage(str_replace(":19132","","[Transfer] Player {$pl->getName()} is transfered to {$cmd[1]}:{$cmd[2]}"));
+                if($player->hasPermission("st.transfer.other")) {
+                    $pl = $this->plugin->getServer()->getPlayer($cmd[3]);
+                    if($pl->isOnline()) {
+                        $this->plugin->transfer($pl, $cmd[1], $cmd[2]);
+                        $this->plugin->getServer()->broadcastMessage(str_replace(":19132","","[Transfer] Player {$pl->getName()} is transfered to {$cmd[1]}:{$cmd[2]}"));
+                    }
                 }
                 else {
                     $player->sendMessage("[Transfer] use /transfer <ip> <port> (player)");
