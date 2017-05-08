1fc141bb348ee3f4ae0b1082a1bf6db41b90b93a
GamakCZ <gamak.mcpe@gmail.com>
2017-05-08 14:26:41 +0200

    Create Main.php

---
+++ WelcomeMessage/src/WelcomeMessage/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WelcomeMessages;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        if(!file_exists($this->getDataFolder())) {
+            @mkdir($this->getDataFolder());
+            $this->getConfig()->set("welcome-message", "Welcome @player on my server...{LINE}- plugin WelcomeMessage by CzechPMDevs");
+            $this->getConfig()->save();
+        }
+
+    }
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $event->setJoinMessage("");
+        $msg = $this->getConfig()->get("welcome-message");
+        $msg = str_replace("{LINE}", "\n", $msg);
+        $msg = str_replace("@player", $player->getName(), $msg);
+        $player->sendMessage($msg);
+    }
+}
