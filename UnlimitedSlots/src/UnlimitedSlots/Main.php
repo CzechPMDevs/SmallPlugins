545b7c498c730f73d105f7587c15efdcb5d8c30b
GamakCZ <gamak.mcpe@gmail.com>
2017-05-20 22:33:30 +0200

    Create Main.php

---
+++ UnlimitedSlots/src/UnlimitedSlots/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace UnlimitedSlots;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\QueryRegenerateEvent;
+use pocketmine\plugin\PluginBase;
+
+class UnlimitedSlots extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    public function onQuerry(QueryRegenerateEvent $event) {
+        $event->setMaxPlayerCount(intval(count($this->getServer()->getOnlinePlayers())+1));
+    }
+}
