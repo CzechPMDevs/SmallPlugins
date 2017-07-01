e4873d49d60fce15438fa5a84961953edc78c549
ImKyd <kralikmatej@seznam.cz>
2017-07-01 15:30:58 +0200

    Update Main.php

--- ServerTransfer/src/ServerTransfer/Main.php
+++ ServerTransfer/src/ServerTransfer/Main.php
@@ -4,6 +4,7 @@ namespace ServerTransfer;
 
 use pocketmine\network\mcpe\protocol\TransferPacket;
 use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
@@ -14,6 +15,15 @@ class Main extends PluginBase {
     public function onEnable() {
         $this->listener = new EventListener($this);
         $this->getServer()->getPluginManager()->registerEvents($this->listener, $this);
+    }
+    
+    public function onDisable(){
+        $players = Server::getInstance()->getOnlinePlayers();
+        if($this->getServer()->isRunning() == false){
+        foreach($players as $p){
+            $this->transfer($p, $this->getConfig()->get("disable-server-ip"), $this->getConfig()->get("disable-server-port"));
+        }
+        }
     }
 
     public function transfer(Player $player, $ip, $port = 19132) {
