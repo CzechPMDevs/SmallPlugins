deeec2fa346e859a727755add0e371f71d095f4d
VixikHD <gamak.mcpe@gmail.com>
2019-01-01 16:03:31 +0100

    namespace + license

--- SimpleHome/src/czechpmdevs/simplehome/Home.php
+++ SimpleHome/src/czechpmdevs/simplehome/Home.php
@@ -1,13 +1,29 @@
 <?php
 
+/**
+ * Copyright (C) 2018-2019  CzechPMDevs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 declare(strict_types=1);
 
-namespace simplehome;
+namespace czechpmdevs\simplehome;
 
 use pocketmine\level\Position;
 use pocketmine\Player;
 use pocketmine\Server;
-use simplehome\event\PlayerHomeTeleportEvent;
+use czechpmdevs\simplehome\event\PlayerHomeTeleportEvent;
 
 /**
  * Class Home
@@ -49,7 +65,7 @@ final class Home extends Position {
      *
      * @return Home
      */
-    public static function fromPosition(Position $position, $name, $player):Home {
+    public static function fromPosition(Position $position, $name, $player): Home {
         return new Home($player, [(int)$position->getX(), (int)$position->getY(), (int)$position->getZ(), $position->getLevel()->getFolderName()], $name);
 
     }
